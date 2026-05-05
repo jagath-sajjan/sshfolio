d07a736089ca8763b8940535b583bb8e14c6a2e5
jagath-sajjan <jagathsajjan097@gmail.com>
2026-05-06 00:05:59 +0530

    fixed vaild main.go

--- main.go
+++ main.go
@@ -1,4 +1,34 @@
-port := os.Getenv("PORT")
-if port == "" {
-		port = "22097"
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	port:= os.Getenv("PORT")
+	if port == "" {
+			port = "22097"
+	}
+
+	listener, err := net.Listen("tcp", ":"+port)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Listening on port", port)
+
+	for {
+		conn, err := listener.Accpet()
+		if err != nil {
+			continue
+		}
+
+		go func(c net.Conn) {
+			defer c.Close()
+			c.Write([]byte("Holaa from jogostation\n"))
+		}(conn)
+	}
 }
